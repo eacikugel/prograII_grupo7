b2d7c7beed36a52b7e75052e513e80510ee3743e
EriGorosito <engorosito@estudiantes.unsam.edu.ar>
2025-09-10 18:11:19 -0300

    Extender Factory Method

--- factory/factory_method/main.py
+++ factory/factory_method/main.py
@@ -4,6 +4,9 @@ def main():
     ny = NYPizzaStore(); chi = ChicagoPizzaStore()
     p1 = ny.order_pizza("cheese"); print("Ethan ordered:", p1)
     p2 = chi.order_pizza("cheese"); print("Joel ordered:", p2)
-
+    p3 = ny.order_pizza("veggie"); print("Juan ordered:", p3)
+    p4 = chi.order_pizza("veggie"); print("Romina ordered:", p4)
+    p5 = ny.order_pizza("pepperoni"); print("Jennifer ordered:", p5)
+    p6 = chi.order_pizza("pepperoni"); print("Graciela ordered:", p6)
 if __name__ == "__main__":
     main()
--- factory/factory_method/pizza.py
+++ factory/factory_method/pizza.py
@@ -5,7 +5,8 @@ class Pizza(ABC):
     toppings: list[str] = []
 
     def prepare(self):
-        print(f"Preparing {self.name}")
+        print(f"\nPreparing {self.name}")
+        print(f"Adding sauce: {self.sauce}")
         print("Adding toppings:", ", ".join(self.toppings))
 
     def bake(self): print("Bake 25 min at 350")
@@ -15,9 +16,41 @@ class Pizza(ABC):
 
 class NYStyleCheesePizza(Pizza):
     def __init__(self):
-        self.name="NY Style Sauce & Cheese"; self.toppings=["Reggiano cheese"]
+        # super().__init__() 
+        self.name="NY Style Sauce & Cheese"; self.sauce="Marinara"; self.toppings=["Reggiano cheese"]
+ 
+class NYStyleVeggiePizza(Pizza):
+    def __init__(self):
+        # super().__init__() 
+        self.name="NY Style Veggie Pizza"; 
+        self.sauce="Marinara";
+        self.toppings=["Vegan Grated Reggiano Cheese", "Mushroom","Onion","Red Pepper"]
+
+class NYStylePepperoniPizza(Pizza):
+    def __init__(self):
+        # super().__init__() 
+        self.name="NY Style Pepperoni Pizza"; 
+        self.sauce="Marinara";
+        self.toppings=["Sliced Pepperoni","Onion","Reaggiano Cheese"]
 
 class ChicagoStyleCheesePizza(Pizza):
     def __init__(self):
-        self.name="Chicago Style Deep Dish Cheese"; self.toppings=["Shredded Mozzarella"]
+        # super().__init__() 
+        self.name="Chicago Style Deep Dish Cheese"; self.sauce="Marinara";self.toppings=["Shredded Mozzarella"]
+    def cut(self): print("Cutting the pizza into square slices")
+
+class ChicagoStyleVeggiePizza(Pizza):
+    def __init__(self):
+        # super().__init__() 
+        self.name="Chicago Style Veggie Pizza"; 
+        self.sauce="Marinara";
+        self.toppings=["Vegan Shredded Mozzarella Cheese", "Black Olives", "Spinach", "Eggplant"]
+    def cut(self): print("Cutting the pizza into square slices")
+
+class ChicagoStylePepperoniPizza(Pizza):
+    def __init__(self):
+        # super().__init__() 
+        self.name="Chicago Style Pepperoni Pizza";
+        self.sauce="Marinara"; 
+        self.toppings=["Shredded Mozzarella Cheese", "Sliced Pepperoni", "Black Olives"]
     def cut(self): print("Cutting the pizza into square slices")
